a75dabed3a9b60a73458ef48b5ec2849b1f12495
Poonam Gupta <pgupta46@in.ibm.com>
2019-07-24 22:17:25 +0530

    initial commit

--- src/csv/com/CsvMain.java
+++ src/csv/com/CsvMain.java
@@ -7,6 +7,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class CsvMain {
@@ -34,15 +36,20 @@ InputStream stream1 = CsvMain.class.getResourceAsStream("/file1.csv");
         }
         
         
-        
-        
+        for(List<String> list : file1List) {
+            for(String  list1 : list) {
+            System.out.println(list1);
+            }
+        }
         
         for(List<String> a : file2List) {
         for(String b : a) {
         System.out.println(b);
+        }      
         }
         
         }
 }
 
-}
+
+
